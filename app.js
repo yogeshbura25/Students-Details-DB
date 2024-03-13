3272dc261a40c618b49a41c475096282ddd265b6
yogesh <burayogesh78@gmail.com>
2024-03-13 13:01:05 +0530

    Student Details Node JS

---
+++ app.js
@@ -0,0 +1,178 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const cors = require("cors");
+app.use(cors());
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const path = require("path");
+const { request } = require("http");
+
+const dbpath = path.join(__dirname, "student.db");
+let db = null;
+
+const initializationDBandServer = async () => {
+    try {
+    db = await open ({
+        filename:dbpath,
+        driver:sqlite3.Database,
+    });
+    app.listen(3000);
+} catch (e) {
+    console.log(`DB error : ${e.message}`);
+    process.exit(1);
+}
+};
+
+initializationDBandServer();
+
+//Get Student Details
+app.get("/studentDetails/", async (request, response) => {
+    const getDetailsQuery = `
+    SELECT
+    *
+    FROM
+    student_db;`;
+    const selectQuery = await db.all(getDetailsQuery);
+    response.send(selectQuery);
+});
+
+//Get Student Details through id
+app.get("/studentDetails/:studentID/", async (request, response) => {
+    const { studentID } = request.params;
+    const getStudentDetails = `
+    SELECT 
+    *
+    FROM
+    student_db
+    WHERE 
+    id = ${studentID};`;
+    const selectIDQuery = await db.get(getStudentDetails);
+    response.send(selectIDQuery); 
+});
+
+//Get Student Details through student_name
+app.get("/studentDetails/name/:name", async (request, response) => {
+    const { name } = request.params;
+    const getNameQuery = `
+    SELECT
+    *
+    FROM
+    student_db
+    WHERE 
+    student_name = "${name}";`;
+    const selectNameQuery = await db.all(getNameQuery);
+    response.send(selectNameQuery);
+});
+
+
+//Get student Details through gender
+app.get("/studentDetails/gender/:gender", async (request, response) => {
+    const { gender } = request.params;
+    const getGenderQuery = `
+    SELECT
+    *
+    FROM
+    student_db
+    WHERE 
+    gender = "${gender}";`;
+    const selectGenderQuery = await db.all(getGenderQuery);
+    response.send(selectGenderQuery);
+});
+
+//Get student Details through Age 
+app.get("/studentDetails/age/:age", async (request, response) => {
+    const { age } = request.params;
+    const getAgeQuery = `
+    SELECT
+    *
+    FROM
+    student_db
+    WHERE 
+    age = ${age};`;
+    const selectAgeQuery = await db.all(getAgeQuery);
+    response.send(selectAgeQuery);
+});
+
+//Get student Details through branch
+app.get("/studentDetails/branch/:branch", async (request, response) => {
+    const { branch } = request.params;
+    const getBranchQuery = `
+    SELECT
+    *
+    FROM
+    student_db
+    WHERE 
+    branch = "${branch}";`;
+    const selectBranchQuery = await db.all(getBranchQuery);
+    response.send(selectBranchQuery);
+});
+
+//Get student Details through rank
+app.get("/studentDetails/rank/:rank", async (request, response) => {
+    const { rank } = request.params;
+    const getRankQuery = `
+    SELECT
+    *
+    FROM
+    student_db
+    WHERE 
+    rank = ${rank};`;
+    const selectRankQuery = await db.all(getRankQuery);
+    response.send(selectRankQuery);
+});
+
+//Post New Student Details In Student_db
+app.post("/NewStudentDetails/", async (request, response) => {
+    const { ID, StudentName, Gender, Age, Branch, Rank, PhoneNumber } = request.body;
+    const AddStudentDetails = `
+    INSERT INTO
+    student_db (id, student_name, gender, age, branch, rank, phone_number)
+    VALUES (
+        ${ID},
+        "${StudentName}",
+        "${Gender}",
+        ${Age},
+        "${Branch}",
+        ${Rank},
+        ${PhoneNumber}
+    );`;
+    await db.run(AddStudentDetails);
+    response.send("Successfully Added New Students");
+});
+
+//Update Student Details
+app.put("/UpdateStudentDetails/", async (request, response) => {
+    const { studentID } = request.params;
+    const { StudentName, Gender, Age, Branch, Rank, PhoneNumber } = request.body;
+    const updateStudentQuery = `
+    UPDATE
+    student_db
+    SET
+        student_name = "${StudentName}",
+        gender = "${Gender}",
+        age = ${Age},
+        branch = "${Branch}",
+        rank = ${Rank},
+        phone_number = ${PhoneNumber}
+    WHERE 
+    id = ${studentID};`;
+    await db.run(updateStudentQuery);
+    response.send("Student Upadted Sucessfully");
+});
+
+//Deleting Student Details
+app.delete("/DeleteStudentDetails/:studentID", async (request, response) => {
+    const { studentID } = request.params;
+    const deleteStudentDetails = `
+    DELETE
+    FROM
+    student_db
+    WHERE
+    id = ${studentID};`;
+    await db.run(deleteStudentDetails);
+    response.send("Student Details Successfully Deteled");
+});
